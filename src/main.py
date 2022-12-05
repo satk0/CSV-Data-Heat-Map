b27e2010d1373d6aba7fcae828c9bfe6bd9f6287
RHubertPL <102611441+RHubertPL@users.noreply.github.com>
2022-12-05 20:27:05 +0100

    Add files via upload

--- src/main.py
+++ src/main.py
@@ -5,7 +5,15 @@ import matplotlib.pyplot as plt
 dataset = pd.read_csv("test.csv", sep=';', index_col=0)
 #dataset = sbn.load_dataset("glue").pivot("Model", "Task", "Score")
 print(dataset)
-sbn.heatmap(dataset)
+
+#od bialego do niebieskiego
+#sbn.heatmap(dataset, cmap="Blues")
+
+#od bialego do czerwonego
+#sbn.heatmap(dataset, cmap="Reds")
+
+#od niebieskiego do czerwonego
+sbn.heatmap(dataset, cmap="RdBu_r")
 
 plt.show()
 
